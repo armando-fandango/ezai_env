7e17e98098a866776bf1c538b4b82202c5813d90
armando-fandango <armando@neurasights.com@users.noreply.github.com>
2020-08-14 16:44:26 -0400

    added miniconda install script

---
+++ install-miniconda.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CONDA_DIR=/opt/conda
+wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh && \
+	/bin/bash Miniconda.sh -f -b -p $CONDA_DIR && \
+	rm Miniconda.sh && \
+  PATH=${CONDA_DIR}/bin:$PATH && \
+  #source $(conda info --base)/etc/profile.d/conda.sh && \
+  conda init
