e59dace41ec08c286bb776c7cf08d7ba458d6e77
armando-fandango <armando@neurasights.com@users.noreply.github.com>
2020-09-02 15:57:14 -0400

    updated windows

--- ezai-conda-create.ps1
+++ ezai-conda-create.ps1
@@ -13,7 +13,7 @@ function ProceedOrExit {
 Write-Host "setting base conda to 4.6.14"
 conda activate base
 conda config --set auto_update_conda False
-conda install -y conda=4.6.14
+conda install -y -S conda=4.6.14
 conda deactivate
 
 Write-Host "creating $venv with python $py_ver ..."
@@ -26,7 +26,7 @@ conda config --env --set channel_priority strict
 conda config --env --remove channels defaults
 conda config --set auto_activate_base false
 
-conda install -y -S -p $venv -c conda-forge "ipython>7.0" jupyter notebook jupyter_contrib_nbextensions jupyter_nbextensions_configurator yapf ipywidgets
+conda install -y -S -p $venv -c conda-forge "ipython>7.0" "notebook>=6.0.0" jupyter_contrib_nbextensions jupyter_nbextensions_configurator yapf ipywidgets
 jupyter nbextension enable code_prettify/code_prettify
 jupyter nbextension enable toc2/main
 jupyter nbextension enable varInspector/main
@@ -36,12 +36,12 @@ jupyter nbextension enable scratchpad/main
 jupyter nbextension enable collapsible_headings/main
 jupyter nbextension enable codefolding/main
 
-conda install -y -S -p $venv -c conda-forge -c defaults cudatoolkit=10.1 cudnn=7.6.5
+conda install -y -S -p $venv -c conda-forge -c defaults "cudatoolkit=10.1" "cudnn=7.6.5"
 #conda install -y -S -p $venv -c conda-forge nccl mpi4py gxx_linux-64 gcc_linux-64
 conda config --env --prepend channels pytorch
 conda config --env --prepend channels fastai
 
-conda install -y -S -p $venv -c fastai -c pytorch -c conda-forge fastai=2.0.0 pytorch=1.6.0 torchvision=0.7.0 "numpy<1.19.0"
+conda install -y -S -p $venv -c fastai -c pytorch -c conda-forge "fastai=2.0.0" "pytorch=1.6.0" "torchvision=0.7.0" "numpy<1.19.0"
 
 conda install -y -S -p $venv -c fastai -c pytorch -c conda-forge --file $condatxt
 # install pip with no-deps so it doesnt mess up conda installed versions
